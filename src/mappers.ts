3ddff571dce68ba06ad48301c094a5652b6aafdc
1Conan <me@1conan.com>
2023-11-14 00:08:20 +0800

    map posterImg

--- src/mappers.ts
+++ src/mappers.ts
@@ -222,6 +222,7 @@ const getVideo = (video: any): Attachment => ({
   srcURL: maxBy((video.video_info.variants as any[]).filter(v => v.content_type === 'video/mp4'), 'bitrate')?.url,
   size: pick(video.original_info, ['width', 'height']),
   isVoiceNote: video.audio_only ? true : null,
+  posterImg: video.media_url,
 })
 const getDynamicPhoto = (photo: any): Attachment => ({
   id: photo.id_str,
